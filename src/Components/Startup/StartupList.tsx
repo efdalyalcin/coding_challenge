bbd21bf90463bfe68ac97501d0ea3259f823005b
efdalyalcin <efdalyalcin@hotmail.com>
2023-12-07 15:18:19 +0100

    solution without cy

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,53 @@
-import { Fragment, ReactElement } from "react";
+import { ReactElement, useEffect, useState } from 'react';
+import { Startup } from '../../Types/Startup';
+import { StartupHttpService } from '../../Http/Startup/Startup.http.service';
+import { Card, CardContent, List, ListItem, Typography } from '@mui/material';
 
 export default function StartupList(): ReactElement {
-  return <Fragment></Fragment>;
+  const [startupsList, setStartupsList] = useState<Startup[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await StartupHttpService.getStartups();
+        setStartupsList([...data]);
+      } catch (error) {
+        console.error('Error fetching startups:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <List id="startup-list">
+      {startupsList.map((startup, index) => (
+        <ListItem key={index}>
+          <Card style={{ width: '100%' }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {startup.name}
+              </Typography>
+              <Typography
+                color="textSecondary"
+                variant="subtitle2"
+                marginBottom={2}
+              >
+                Founded: {new Date(startup.dateFounded).getFullYear()}
+                <span> | </span>
+                <span>{startup.employees} Employees</span>
+                <span> | </span>
+                <span>{startup.totalFunding} $</span>
+                <span> | </span>
+                <span>{startup.currentInvestmentStage}</span>
+              </Typography>
+              <Typography variant="body2" component="div">
+                {startup.shortDescription}
+              </Typography>
+            </CardContent>
+          </Card>
+        </ListItem>
+      ))}
+    </List>
+  );
 }
