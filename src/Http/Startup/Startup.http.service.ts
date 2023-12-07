2e36f7ea8923ad20cab7cc751f409f9d0b264b89
efdalyalcin <efdalyalcin@hotmail.com>
2023-12-07 15:44:39 +0100

    service request fixed according to test

--- src/Http/Startup/Startup.http.service.ts
+++ src/Http/Startup/Startup.http.service.ts
@@ -9,7 +9,13 @@ export class StartupHttpService {
   }
 
   public static async getStartups(): Promise<Startup[]> {
-    const response = await axios.get<StartupDTO>(`/api/startups`);
-    return Object.values(response.data);
+    const response = await axios.get<StartupDTO[]>(`/api/startups`);
+
+    // StartupMapper is needed according to testes, so I could only think this in short time
+    // this creates O(n^2) time complexity which is not good.
+    const mappedStartups = response.data.map((startupDTO) =>
+      StartupMapper.map(startupDTO)
+    );
+    return mappedStartups;
   }
 }
